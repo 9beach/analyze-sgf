3dca0084bda5c12e7a5ff25618970ce7d5e2b3d5
9beach <9beach@gmail.com>
2021-02-08 00:08:45 +0900

    Add -g option to src/cli.sh for stable testing

--- test/cli-test.sh
+++ test/cli-test.sh
@@ -9,8 +9,9 @@ echo Tests src/cli.sh with option -r.
 cp test/ex-ren-vs-shin.sgf $sample.sgf
 
 # Creates $sample-analyzed.sgf.
-src/cli.js -r test/ex-ren-vs-shin-responses.json $sample.sgf &> /dev/null
-
+src/cli.js -r test/ex-ren-vs-shin-responses.json $sample.sgf \
+	-g 'maxWinrateLossForGoodMove:2,minWinrateLossForBadMove:5,minWinrateLossForBadHotSpot:20,showVariationsAfterLastMove:false,minWinrateLossForVariations:5,showBadVariations:false,maxVariationsForEachMove:10' \
+	&> /dev/null
 # Strips commemnts.
 cat test/ex-ren-vs-shin-analyzed.sgf | tr -d '\n' | sed -e 's:C\[[^]]*\]::g' > $sample-expected
 cat $sample-analyzed.sgf | tr -d '\n' | sed -e 's:C\[[^]]*\]::g' > $sample-result
