b83dd4c5b02f7c181caf52a3f3234d8e60c291ec
9beach <9beach@gmail.com>
2021-02-12 11:10:22 +0900

    Fix temporary file referencing error

--- test/index.test.sh
+++ test/index.test.sh
@@ -2,14 +2,16 @@
 
 set -e
 
+######################
+# Setup test fixtures.
+######################
+test=$(mktemp)
+cp test/t-ren-vs-shin.sgf $test.sgf
+
 #########
 # Test 1.
 #########
 echo -n Tests src/index.js with option -r.
-
-test=$(mktemp)
-
-cp test/t-ren-vs-shin.sgf $test.sgf
 
 # Creates $test-analyzed.sgf.
 src/index.js -f test/t-ren-vs-shin-responses.json \
@@ -30,10 +32,6 @@ echo -e "\033[1;32m Ok \033[0m"
 #########
 echo -n Tests src/index.js with option -k and \"cat\".
 
-test=$(mktemp)
-
-cp test/t-ren-vs-shin.sgf $test.sgf
-
 # Creates $test-analyzed.sgf.
 src/index.js -k 'path:"cat",arguments:"test/t-ren-vs-shin-responses.json"' \
 	-g 'maxWinrateLossForGoodMove:2,minWinrateLossForBadMove:5,minWinrateLossForBadHotSpot:20,showVariationsAfterLastMove:false,minWinrateLossForVariations:5,showBadVariations:false,maxVariationsForEachMove:10' \
@@ -53,13 +51,16 @@ echo -e "\033[1;32m Ok \033[0m"
 #########
 echo -n Tests src/index.js with option -k and \"katago-error.sh\".
 
-src/index.js -k 'path:"test/katago-error.sh",arguments:""' debug-baduk-ren-vs-shin.sgf 2> $test-result || true
+src/index.js -k 'path:"test/katago-error.sh",arguments:""' $test.sgf 2> $test-result || true
 
-diff test/t-katago-error-responses.txt $test-result
+grep '  path: "test/katago-error.sh"' $test-result &> /dev/null
+grep '  arguments: "' $test-result &> /dev/null
+grep 'This is test response of KataGo error' $test-result &> /dev/null
+grep 'This is error text' $test-result &> /dev/null
 
 echo -e "\033[1;32m Ok \033[0m"
 
-#########
-# Removes test fixtures.
-#########
+#########################
+# Teardown test fixtures.
+#########################
 rm -f $test-*
