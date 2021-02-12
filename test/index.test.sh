78a863e45ee1bcfe3f59fd47f4936eba6d7b5e96
9beach <9beach@gmail.com>
2021-02-12 23:02:29 +0900

    Rename `test` to `temp`

--- test/index.test.sh
+++ test/index.test.sh
@@ -8,25 +8,25 @@ cd $REPO_PATH
 ######################
 # Setup test fixtures.
 ######################
-test=$(mktemp)
-cp test/t-ren-vs-shin.sgf $test.sgf
+temp=$(mktemp)
+cp test/t-ren-vs-shin.sgf $temp.sgf
 
 #########
 # Test 1.
 #########
 echo -n Tests src/index.js with option -r.
 
-# Creates $test-analyzed.sgf.
+# Creates $temp-analyzed.sgf.
 src/index.js -f test/t-ren-vs-shin-responses.json \
 	-g 'maxWinrateLossForGoodMove:2,minWinrateLossForBadMove:5,minWinrateLossForBadHotSpot:20,showVariationsAfterLastMove:false,minWinrateLossForVariations:5,showBadVariations:false,maxVariationsForEachMove:10' \
-	$test.sgf &> /dev/null
+	$temp.sgf &> /dev/null
 
 # Strips commemnts.
-cat test/t-ren-vs-shin-analyzed.sgf | tr -d '\n' | sed -e 's:C\[[^]]*\]::g' > $test-expected
-cat $test-analyzed.sgf | tr -d '\n' | sed -e 's:C\[[^]]*\]::g' > $test-result
+cat test/t-ren-vs-shin-analyzed.sgf | tr -d '\n' | sed -e 's:C\[[^]]*\]::g' > $temp-expected
+cat $temp-analyzed.sgf | tr -d '\n' | sed -e 's:C\[[^]]*\]::g' > $temp-result
 
 # Compares them.
-diff $test-expected $test-result
+diff $temp-expected $temp-result
 
 echo -e "\033[1;32m Ok \033[0m"
 
@@ -35,17 +35,17 @@ echo -e "\033[1;32m Ok \033[0m"
 #########
 echo -n Tests src/index.js with option -k and \"cat\".
 
-# Creates $test-analyzed.sgf.
+# Creates $temp-analyzed.sgf.
 src/index.js -k 'path:"cat",arguments:"test/t-ren-vs-shin-responses.json"' \
 	-g 'maxWinrateLossForGoodMove:2,minWinrateLossForBadMove:5,minWinrateLossForBadHotSpot:20,showVariationsAfterLastMove:false,minWinrateLossForVariations:5,showBadVariations:false,maxVariationsForEachMove:10' \
-        $test.sgf &> /dev/null
+        $temp.sgf &> /dev/null
 
 # Strips commemnts.
-cat test/t-ren-vs-shin-analyzed.sgf | tr -d '\n' | sed -e 's:C\[[^]]*\]::g' > $test-expected
-cat $test-analyzed.sgf | tr -d '\n' | sed -e 's:C\[[^]]*\]::g' > $test-result
+cat test/t-ren-vs-shin-analyzed.sgf | tr -d '\n' | sed -e 's:C\[[^]]*\]::g' > $temp-expected
+cat $temp-analyzed.sgf | tr -d '\n' | sed -e 's:C\[[^]]*\]::g' > $temp-result
 
 # Compares them.
-diff $test-expected $test-result
+diff $temp-expected $temp-result
 
 echo -e "\033[1;32m Ok \033[0m"
 
@@ -54,16 +54,16 @@ echo -e "\033[1;32m Ok \033[0m"
 #########
 echo -n Tests src/index.js with option -k and \"katago-error.sh\".
 
-src/index.js -k 'path:"test/katago-error.sh",arguments:""' $test.sgf 2> $test-result || true
+src/index.js -k 'path:"test/katago-error.sh",arguments:""' $temp.sgf 2> $temp-result || true
 
-grep '  path: "test/katago-error.sh"' $test-result &> /dev/null
-grep '  arguments: "' $test-result &> /dev/null
-grep 'This is KataGo error test' $test-result &> /dev/null
-grep 'This is error message' $test-result &> /dev/null
+grep '  path: "test/katago-error.sh"' $temp-result &> /dev/null
+grep '  arguments: ""' $temp-result &> /dev/null
+grep 'This is KataGo error test.' $temp-result &> /dev/null
+grep 'This is error message.' $temp-result &> /dev/null
 
 echo -e "\033[1;32m Ok \033[0m"
 
 #########################
 # Teardown test fixtures.
 #########################
-rm -f $test-*
+rm -f $temp-*
