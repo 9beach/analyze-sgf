5a6ba1bc7f965bcb6988c20a4bdc1b1f354f5d12
9beach <9beach@gmail.com>
2021-03-15 21:34:32 +0900

    Add unit test cases

--- test/node.test.js
+++ test/node.test.js
@@ -8,15 +8,17 @@ const yamlpath = require.resolve('../src/analyze-sgf.yml');
 const opts = yaml.load(fs.readFileSync(yamlpath));
 const sgfopts = opts.sgf;
 
-describe('Node', () => {
+describe('Node.setWinrate', () => {
   it('should result expected pl.', () => {
     const node = new Node('(;B[aa];W[bb])');
+
     assert.equal(node.pl, 'B');
   });
 
   it('should result expected winrate.', () => {
     const node = new Node('(;B[aa];W[bb])');
     const currinfo = { winrate: 0.44, scoreLead: 6.5, visits: 1000 };
+
     node.setWinrate(null, currinfo, sgfopts);
 
     assert.equal(node.winrateDrop, undefined);
@@ -51,5 +53,22 @@ describe('Node', () => {
     assert.equal(node.scoreDrop.toFixed(1), 1.5);
     assert.equal(node.myScoreLead.toFixed(1), -6.5);
     assert.equal(node.myWinrate.toFixed(2), 0.56);
+  });
+});
+
+describe('Node.formatPV', () => {
+  it('should result expected PVs.', () => {
+    const node = [new Node('(;B[aa];W[bb];B[cc])'), new Node(';B[aa]')];
+    const previnfo = { winrate: 0.4, scoreLead: 5.0, visits: 1000 };
+    const currinfo = { winrate: 0.44, scoreLead: 6.5, visits: 1000 };
+
+    node[0].setWinrate(previnfo, currinfo, sgfopts);
+    assert.equal(
+      node[0].formatPV(),
+      'BA19 B18 C17 (W 56.00%, B 6.50, 1000 visits)',
+    );
+
+    node[1].setWinrate(previnfo, currinfo, sgfopts);
+    assert.equal(node[1].formatPV(), 'A19 (W 56.00%, B 6.50, 1000 visits)');
   });
 });
